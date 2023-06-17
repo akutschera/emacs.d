e78e2e10948095bf75bb62b9ebde7bfc66b26b1b
Andreas Kutschera <andreas.kutschera@web.de>
2023-06-17 14:40:34 +0200

    feat: use tempdir for editing backups

--- customizations/editing.el
+++ customizations/editing.el
@@ -4,3 +4,8 @@
   (setq global-company-mode 1)
   )
 
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+(setq make-backup-files nil) ;; TODO: Braucht's das noch?
