edccab3190d764daa82107fdc6939300822900e4
Andreas Kutschera <andreas.kutschera@web.de>
2023-06-19 18:50:38 +0200

    feat: create missing test files on demand

--- customizations/projects.el
+++ customizations/projects.el
@@ -3,5 +3,7 @@
 (use-package projectile
   :init
   (projectile-mode +1)
+  :config
+  (setq projectile-create-missing-test-files 1)
   :bind (:map projectile-mode-map
               ("s-p" . projectile-command-map)))
