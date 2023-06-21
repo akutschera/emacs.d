875b2dd47bdbee7b5240512f43029acfdc802789
Andreas Kutschera <andreas.kutschera@web.de>
2023-06-21 18:02:47 +0200

    feat: allow folding when editing

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -5,3 +5,13 @@
 
 (setq  xref-auto-jump-to-first-xref t)
 (setq xref-prompt-for-identifier nil) ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Identifier-Search.html
+
+;; code folding: https://github.com/gregsexton/origami.el
+(use-package origami
+  :config
+   (define-key origami-mode-map (kbd "s-o o") 'origami-open-node)
+   (define-key origami-mode-map (kbd "s-o s-o") 'origami-open-node)
+   (define-key origami-mode-map (kbd "s-o c") 'origami-close-node)
+   (define-key origami-mode-map (kbd "s-o s-c") 'origami-close-node)
+   :init
+  (global-origami-mode t))
